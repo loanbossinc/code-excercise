7e45678331bf29a23c2f4b6cd89263e6898bd895
Austin Baird <austinzbaird@gmail.com>
2021-11-06 23:23:46 -0400

    Adding Routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,1 +1,4 @@
-export const ROUTE_APP_LANDING = "/example";
+export const ROUTE_APP_LANDING = "/";
+export const ROUTE_USER_PROFILE = "/profile";
+export const ROUTE_USER_PROFILE_EDIT = "/profile-edit";
+export const ROUTE_USER_CONTACT_INFO_EDIT = "/contact-info-edit";
